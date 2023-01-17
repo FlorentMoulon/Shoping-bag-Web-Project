1252c97f3696927fd3accafa44495eafe6272815
Damidas <damidas19@gmail.com>
2023-01-17 16:34:27 +0100

    fin carousel

--- views/shared/v_navbar.php
+++ views/shared/v_navbar.php
@@ -3,8 +3,10 @@
     <div class="navbar-header">
       <a class="navbar-brand" href="index.php">ISIWEB4SHOP</a>
     </div>
-
-    <div class="navbar-collapse" id="navbarColor01">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav me-auto">
         <li class="nav-item"><a class="nav-link active" href="index.php">Accueil</a></li>
         <li class="nav-item"><a class="nav-link active" href="index.php?action=boissons">Boissons</a></li>
@@ -12,18 +14,19 @@
         <li class="nav-item"><a class="nav-link active" href="index.php?action=fruitsSecs">Fruits Secs</a></li>
       </ul>
 
-      <ul class="float-right navbar-nav ms-md-auto">
+      <ul class="float-right navbar-nav ms-md-auto pull-right">
         <?php
         if (isset($_SESSION['id'])) {
           if (isset($_GET['action']) and $_GET['action'] == 'deconnexion') {
-            echo "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"index.php?action=connexion\">Connexion</a></li>";
+            echo "<li class=\"nav-item\"><a class=\"nav-link active pull-right\" href=\"index.php?action=connexion\">Connexion</a></li>";
           } else {
-            echo "<li class=\"nav-item\"><a class=\"nav-link dropdown-toggle show\" data-bs-toggle=\"dropdown\" href=\"index.php?action=moncompte\"role=\"button\" aria-haspopup=\"true\" aria-expanded=\"true\">Mon compte</a></li>";
-          }
-        } else echo "<li class=\"nav-item\"><a class=\"nav-link active\" href=\"index.php?action=connexion\">Connexion</a></li>";
+            echo "<li class=\"nav-item\"><a class=\"nav-link active pull-right\" href=\"index.php?action=moncompte\">Mon compte</a></li>";
+            //echo "<li class=\"nav-item\"><a class=\"nav-link dropdown-toggle show\" data-bs-toggle=\"dropdown\" href=\"index.php?action=moncompte\"role=\"button\" aria-haspopup=\"true\" aria-expanded=\"true\">Mon compte</a></li>";
+        }
+        } else echo "<li class=\"nav-item\"><a class=\"nav-link active pull-right\" href=\"index.php?action=connexion\">Connexion</a></li>";
         ?>
 
-        <li class="nav-item"><a class="nav-link active" href="index.php?action=panier">Panier</a></li>
+        <li class="nav-item"><a class="nav-link active pull-right" href="index.php?action=panier"><!--<img src="assets/otherimages/panier.png" alt = "Panier">-->Panier</a></li>
       </ul>
     </div>
   </div>
--- views/v_home.php
+++ views/v_home.php
@@ -16,29 +16,39 @@
     <button type="button" data-bs-target="#Accueil" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#Accueil" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="2000">
-      <img src="assets/otherimages/biscuits.png" class="d-block w-100" alt="Image de biscuits">
-      <div class="carousel-caption d-none d-md-block">
-        <h2>BISCUITS</h2>
-        <p>Notre gamme biscuit.</p>
-      </div>
+        <a href="index.php?action=biscuits">
+            <img src="assets/otherimages/biscuits.png" class="d-block w-100" alt="Image de biscuits">
+            <div class="carousel-caption d-none d-md-block">
+                <h2>BISCUITS</h2>
+                <p>Notre gamme biscuit.</p>
+            </div>    
+        </a>  
     </div>
+    
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="assets/otherimages/boissons.png" class="d-block w-100" alt="Image de boissons">
-      <div class="carousel-caption d-none d-md-block">
-        <h2>BOISSONS</h2>
-        <p>L'ensemble de nos boissons pour vous rafraichir.</p>
-      </div>
+        <a href="index.php?action=boissons">
+            <img src="assets/otherimages/boissons.png" class="d-block w-100" alt="Image de boissons">
+            <div class="carousel-caption d-none d-md-block">
+                <h2>BOISSONS</h2>
+                <p>L'ensemble de nos boissons pour vous rafraichir.</p>
+            </div>
+        </a>
     </div>
+    
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="assets/otherimages/fruits_sec.png" class="d-block w-100" alt="Image de fruits sec">
-      <div class="carousel-caption d-none d-md-block">
-        <h2>FRUITS SEC</h2>
-        <p>Retrouvez toutes notre offres fruits sec pour vos en cas.</p>
-      </div>
+        <a href="index.php?action=fruitsSecs">
+            <img src="assets/otherimages/fruits_sec.png" class="d-block w-100" alt="Image de fruits sec">
+            <div class="carousel-caption d-none d-md-block">
+                <h2>FRUITS SEC</h2>
+                <p>Retrouvez toutes notre offres fruits sec pour vos en cas.</p>
+            </div>
+        </a>
     </div>
   </div>
+  
   <button class="carousel-control-prev" type="button" data-bs-target="#Accueil" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
